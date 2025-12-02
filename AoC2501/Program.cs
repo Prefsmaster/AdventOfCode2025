9d3f664248f1489437b15f2dbb5db876b85ebff2
Ferenc <ferencn@gmail.com>
2025-12-02 16:31:25 +0100

    removed unnecessary variable

--- AoC2501/Program.cs
+++ AoC2501/Program.cs
@@ -7,15 +7,14 @@ var crossings = 0L;
 
 foreach (var rotation in rotations)
 {
-    var distance = long.Parse(rotation[1..]) ;
-    var delta = distance * (rotation[0] == 'R' ? 1 : -1);
-
+    var steps = long.Parse(rotation[1..]) ;
     // add 0-crossings for full rotations
-    crossings += distance / 100;
+    crossings += steps / 100;
+    steps *= rotation[0] == 'R' ? 1 : -1;
 
     // save old position and update arrow position
     var oldarrow = arrow;
-    arrow = (arrow + delta)%100;
+    arrow = (arrow + steps)%100;
     // keep in range 0..99
     if (arrow < 0) arrow += 100;
 
@@ -23,8 +22,8 @@ foreach (var rotation in rotations)
         atzeros++;
     else
         // when not landed on 0 and started from 0, and when we end up higher after a left,
-        // or lower after a right, the partial rotation crossed zero and must be counted.
-        if ((oldarrow != 0) && (delta < 0 && arrow > oldarrow) || (delta > 0 && arrow < oldarrow)) 
+        // or lower after a right, the partial rotation crossed zero and must be counted!
+        if ((oldarrow != 0) && (steps < 0 && arrow > oldarrow) || (steps > 0 && arrow < oldarrow)) 
             crossings++;   
 }
 Console.WriteLine($"Password1: {atzeros}");
