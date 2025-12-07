66ba09c12226c51e882d9ff20f0754526c64b20a
Ferenc <ferencn@gmail.com>
2025-12-07 18:19:29 +0100

    Added some explanatory comments.

--- AoC2507/Program.cs
+++ AoC2507/Program.cs
@@ -9,7 +9,7 @@ var characterarray = new List<char[]>();
 foreach (var line in lines)
     characterarray.Add(line.ToCharArray());
 // part2: remember how many paths lead to each node
-var pathsleadingtonode = new ulong[lineLen];
+var pathsleadingtoposition = new ulong[lineLen];
 
 // place a | under the S
 for (var i = 0; i < lineLen; i++)
@@ -17,13 +17,13 @@ for (var i = 0; i < lineLen; i++)
     if (characterarray[0][i] == 'S')
     {
         characterarray[1][i] = '|';
-        pathsleadingtonode[i]++;
+        pathsleadingtoposition[i]++;
         break;
     }
 }
 // then for each line, place 2 | symbols under each ^ that has a | above it, replace ^ with *.
 // count all * with a | above it.
-var splits = 0;
+var splitCount = 0;
 for (var line = 2; line<characterarray.Count; line += 2)
 {
     for (var i = 0; i < lineLen; i++)
@@ -32,34 +32,38 @@ for (var line = 2; line<characterarray.Count; line += 2)
         {
             if (characterarray[line][i] == '^')
             {
-                splits++;
+                splitCount++;
                 characterarray[line][i - 1] = '|';
                 characterarray[line][i + 1] = '|';
                 characterarray[line + 1][i - 1] = '|';
                 characterarray[line + 1][i + 1] = '|';
-                // propagate paths to left and right, joining beams means adding paths 
-                pathsleadingtonode[i - 1] += pathsleadingtonode[i];
-                pathsleadingtonode[i + 1] += pathsleadingtonode[i];
-                pathsleadingtonode[i] = 0;
+                // propagate paths down to left and right
+                // joining beams by adding them 
+                pathsleadingtoposition[i - 1] += pathsleadingtoposition[i];
+                pathsleadingtoposition[i + 1] += pathsleadingtoposition[i];
+                // propagate down zero ways to reach this position
+                // meam was split!
+                pathsleadingtoposition[i] = 0;
             }
             else
             {
+                // just propagate down
                 characterarray[line][i] = '|';
                 characterarray[line + 1][i] = '|';
             }
         }
     }
 }
-Console.WriteLine(splits);
+Console.WriteLine(splitCount);
 
 // part 2 sum all paths leading to the final nodes
 var ans2 = 0UL;
 for (var i = 0; i < lineLen; i++)
 {
-    ans2 += pathsleadingtonode[i];
+    ans2 += pathsleadingtoposition[i];
 }
 
 Console.WriteLine(ans2);
-
+// show resulting path diagram
 foreach (var res in characterarray)
     Console.WriteLine(new string(res));
