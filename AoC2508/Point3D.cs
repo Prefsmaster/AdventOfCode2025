40a86e8ce6505e01d94fd2adebac89b2a78051bf
Ferenc <ferencn@gmail.com>
2025-12-08 13:37:45 +0100

    Classes to separate files.

---
+++ AoC2508/Point3D.cs
@@ -0,0 +1,15 @@
+﻿public class Point3D(ulong x, ulong y, ulong z)
+{
+    public ulong X { get; set; } = x;
+    public ulong Y { get; set; } = y;
+    public ulong Z { get; set; } = z;
+
+    public float DistanceTo(Point3D other)
+    {
+        return MathF.Sqrt(
+            (X - other.X) * (X - other.X) +
+            (Y - other.Y) * (Y - other.Y) +
+            (Z - other.Z) * (Z - other.Z)
+        );
+    }
+}
--- AoC2508/Program.cs
+++ AoC2508/Program.cs
@@ -6,14 +6,14 @@ foreach (var line in lines)
     var coords = line.Split(',').Select(ulong.Parse).ToArray();
     junctionBoxes.Add(new Point3D(coords[0], coords[1], coords[2]));
 }
-var pairsPerDistance = CreatePairs(junctionBoxes.ToArray()).OrderBy(p => p.Distance).ToArray();
+var pairsPerDistance = CreatePairs([.. junctionBoxes]).OrderBy(p => p.Distance).ToArray();
 
 Point3DPair lastConnected = pairsPerDistance[0];
 var Groups = new List<List<Point3D>>();
 for (var i=0; i<pairsPerDistance.Length; i++)
 {
-    // Print Part 1 solution after 1000 pairs are processed
-    if (i == 1000)
+    // Print Part 1 solution after 1000 (for real data) or 10 (testdata) are processed
+    if ((i == 1000 && pairsPerDistance.Length>1000) || (i == 10 && pairsPerDistance.Length < 1000))
     {
         // sort groups by size descending, take top 3, multiply their sizes
         var ordered = Groups.OrderByDescending(g => g.Count).Take(3).ToArray();
@@ -29,8 +29,8 @@ for (var i=0; i<pairsPerDistance.Length; i++)
     // both not found? create new group containing the two boxes
     if (g1 == null && g2 == null)
     {
-        Groups.Add(new List<Point3D>(pair.Points));
-        lastConnected = pair;         // remember last processed pair for Part 2
+        Groups.Add([.. pair.Points]);
+        lastConnected = pair; // remember last processed pair for Part 2
     }
     else
     {
@@ -69,35 +69,4 @@ static List<Point3DPair> CreatePairs(Point3D[] points)
         }
     }
     return allPairs;
-}
-
-public class Point3D(ulong x, ulong y, ulong z)
-{
-    public ulong X { get; set; } = x;
-    public ulong Y { get; set; } = y;
-    public ulong Z { get; set; } = z;
-
-    public float DistanceTo(Point3D other)
-    {
-        return MathF.Sqrt(
-            (X - other.X) * (X - other.X) +
-            (Y - other.Y) * (Y - other.Y) +
-            (Z - other.Z) * (Z - other.Z)
-        );
-    }
-}
-
-class Point3DPair
-{
-    public Point3D[] Points { get; set; }
-    public float Distance { get; set; }
-    public Point3DPair(Point3D p1, Point3D p2)
-    {
-        Points = [p1, p2];
-        Distance = p1.DistanceTo(p2);
-    }
-
-    // For debugging, helps to see the pair and distance
-    public override string ToString()
-        => $"({Points[0].X},{Points[0].Y},{Points[0].Z})+({Points[1].X},{Points[1].Y},{Points[1].Z}) Dist: {Distance}";
 }
